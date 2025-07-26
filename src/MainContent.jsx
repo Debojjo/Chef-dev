1ae520cc8ac96ca8f203546cf904e3da86cc048c
Debojjo Talukdar <talukdardebojjo26@@gmail.com>
2025-07-26 12:14:58 +0530

    Added scroll into view

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Recipe from "./Recipe.jsx";
 import { getRecipeFromAi } from "./ai";
 
@@ -7,6 +7,7 @@ export default function MainContent() {
   const [recipe, setRecipe] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const loadingSection  = useRef(null);
 
   const ingredientsList = newIngredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
@@ -21,6 +22,12 @@ export default function MainContent() {
     ]);
   }
 
+  useEffect(() => {
+    if(recipe !== "" && loadingSection.current !==null) {
+      loadingSection.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [recipe])
+
   async function handleGenerateRecipe() {
     setLoading(true);
     setError(null);
@@ -33,7 +40,7 @@ export default function MainContent() {
       setError("Failed to generate recipe.");
       console.error(error);
     } finally {
-      setLoading(false); // ✅ This ensures loading ends even if there's an error
+      setLoading(false); 
     }
   }
 
@@ -54,7 +61,7 @@ export default function MainContent() {
           <ul className="ingredients-list">{ingredientsList}</ul>
           {ingredientsList.length > 4 && (
             <div className="recipe-container">
-              <div>
+              <div ref={loadingSection}>
                 <h3>It's time to generate a recipe!</h3>
                 <p>
                   You're all set! Tap the button below to generate your recipe!
